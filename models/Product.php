c4b291af1cf58614862eaf69a44632cc05507f41
AyaMohammed <engayasharafelden@gmail.com>
2016-02-18 22:00:25 +0200

    Update Product.php

--- models/Product.php
+++ models/Product.php
@@ -1,5 +1,5 @@
-t<?php
-include DbConnection;
+<?php
+
 /**
  * Created by PhpStorm.
  * User: ayasharafelden
@@ -7,140 +7,100 @@ include DbConnection;
  * Time: 7:29 AM
  */
 class Product
+
 {
+
+private $dbConnection;
     ///////Add Product to product table post method////////////////////////////////////////////////////////
-    ////////////////////asking for something we need in product table to enter the name of category not the id
-    function insert_product(){
-        DbConnection::getConnection();
-        $sql = "INSERT INTO products (p_name,u_price,ctg_id,p_img);
-                 VALUES ('".$_POST["p_name"]."', '".$_POST["u_price"]."', '".$_POST["ctg_id"]."','".$_POST["p_img"]."')";
+	function __construct(mysqli $dbCon)
+    {
+        ////////////////////asking for something we need in product table to enter the name of category not the id
+        $this->dbConnection = $dbCon;
+    }
+
+        public  function insert_product(){
+        $sql = "INSERT INTO products (p_name,u_price,ctg_id,p_img)
+                 VALUES ('".$_POST["p_name"]."', '".$_POST["u_price"]."', '".$_POST["ctg_id"]."','".$_FILES['p_img']['name']."')";
 
 
-        if ( DbConnection::getConnection()->query($sql) === TRUE) {
-            echo "New record created successfully";
+        if ($this->dbConnection->query($sql) === TRUE) {
+             
+          header("location: add_product.html");
+
         } else {
-            echo "Error: " . $sql . "<br>" . DbConnection::getConnection()->query($sql) ->error;
+            echo "Error: " . $sql . "<br>" . $this->dbConnection->query($sql)->error;
         }
     }
     /////////////////////////////////////////////////////////Second function/////////////////////////////////////////////////
     ///////////////////////////////////////////disable status///////////////////////////////////////////////////////////////
-    function disable_status(){
+ public  function disable_status(){
         ///////////////////post id of user and product status post method
-        $sql = "UPDATE orders SET  status='".$_POST["p_status"]."' WHERE id='".$_POST["u_id"]."'";
+        $sql = "UPDATE products SET  status='".$_POST["p_status"]."' WHERE id='".$_POST["u_id"]."'";
 
-        if (DbConnection::getConnection()->query($sql) === TRUE) {
+        if ($this->dbConnection->query($sql) === TRUE) {
             echo "Record updated successfully";
         } else {
-            echo "Error updating record: " . DbConnection::getConnection()->error;
+            echo "Error updating record: " . $this->dbConnection->error;
         }
 
-
-      DbConnection::closeConnection();
-
-
-    }
+ }
     //////////////////////////////////////////////////////////Third Function///////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////Update Product post method////////////////////////////////////////////////////////
-    function update_product(){
+   public function update_product($v,$v1,$v2,$v3,$v4){
         //////////////////////////////////////update by name////////////////////////////////////////////////////////////////
         //////////////////////////////////////if will update price////////////////////////////////////////////////////////
-        if($_POST["u_price"]){
-            $sql = "UPDATE product SET  u_price='".$_POST["u_price"]."' WHERE id='".$_POST["p_id"]."'";
+       
+            $sql = "UPDATE products SET  p_name='".$v1."', u_price='".$v2."',ctg_id='".$v3."' ,p_img='".$v4."'  WHERE p_id='".$v."'";
 
-            if (DbConnection::getConnection()->query($sql) === TRUE) {
+            if ($this->dbConnection->query($sql) === TRUE) {
                 echo "Record updated successfully";
             } else {
-                echo "Error updating record: " . DbConnection::getConnection()->error;
+                echo "Error updating record: " .$this->dbConnection->query($sql)->error;
             }
-            DbConnection::closeConnection();
-        }
+
+        
         //////////////////////////////////////////////////////////Update Name/////////////////////////////////////////////////
 
-       else if($_POST["p_name"]){
-            $sql = "UPDATE products SET  p_name='".$_POST["p_name"]."' WHERE id='".$_POST["p_id"]."'";
-
-            if (DbConnection::getConnection()->query($sql) === TRUE) {
-                echo "Record updated successfully";
-            } else {
-                echo "Error updating record: " . DbConnection::getConnection()->error;
-            }
-            DbConnection::closeConnection();
-        }
-       ////////////////////////////////////////////////////////Update Product Image///////////////////////////////
-        else if($_POST["p_img"]){
-            $sql = "UPDATE products SET  p_img='".$_POST["p_img"]."' WHERE id='".$_POST["p_id"]."'";
-
-            if (DbConnection::getConnection()->query($sql) === TRUE) {
-                echo "Record updated successfully";
-            } else {
-                echo "Error updating record: " . DbConnection::getConnection()->error;
-            }
-            DbConnection::closeConnection();
-          }
+       
       }
 //////  //  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////Function Delete Product TYpe of Method is Post////////////////////////////////
-function  delete_product(){
-    DbConnection::getConnection();
-    $sql = "DELETE FROM products WHERE p_id='".$_POST["p_id"]."'";
-    if ( DbConnection::getConnection()->query($sql) === TRUE) {
-        echo "New record created successfully";
+public function  delete_product($v){
+
+    $sql = "DELETE FROM products WHERE p_id='".$v."'";
+    if ( $this->dbConnection->query($sql) === TRUE) {
+        echo " successfully";
     } else {
-        echo "Error: " . $sql . "<br>" . DbConnection::getConnection()->query($sql) ->error;
+        echo "Error: " . $sql . "<br>" . $this->dbConnection->query($sql)->error;
     }}
     /////////////////////////////////////////////////Function Get Products Post Method/////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function Get_Products(){
-    DbConnection::getConnection();
-    $sql = "Select * FROM products";
-    if ( DbConnection::getConnection()->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . DbConnection::getConnection()->query($sql) ->error;
-    }
+public function Get_Products(){
+    $sql = "select * from products";
+
+    $result = $this->dbConnection->query($sql);
+    return $result;
 
 }
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////Function Get Specific Product (Search)///////////////////////////////////////////////////
-    function Search_product(){
+  public  function Search_product($value){
         //////////////////////////////////////////Search using Product Id////////////////////////////////////////////////////////////////
-        if($_POST["p_id"]){
-        DbConnection::getConnection();
-        $sql = "Select * FROM products where p_id='".$_POST["p_id"]."'";
-        if ( DbConnection::getConnection()->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . DbConnection::getConnection()->query($sql) ->error;
-        }}
-        //////////////////////////////////////////////////Search using Category////////////////////////////////////////////////////////
-       else if($_POST["ctg_id"]){
-            DbConnection::getConnection();
-            $sql = "Select * FROM products where ctg_id='".$_POST["ctg_id"]."'";
-            if ( DbConnection::getConnection()->query($sql) === TRUE) {
-                echo "New record created successfully";
-            } else {
-                echo "Error: " . $sql . "<br>" . DbConnection::getConnection()->query($sql) ->error;
-            }
-       }
-       else if($_POST["p_name"]){
-           DbConnection::getConnection();
-           $sql = "Select * FROM products where p_name='".$_POST["p_name"]."'";
-           if ( DbConnection::getConnection()->query($sql) === TRUE) {
-               echo "New record created successfully";
-           } else {
-               echo "Error: " . $sql . "<br>" . DbConnection::getConnection()->query($sql) ->error;
-           }
-       }
+        $sql = "select * FROM products where p_id='".$value."'";
+        $result=$this->dbConnection->query($sql);
+        return $result;
+
+   
 
   }
     /////////////////////////////////////////////////////Get Name of Product//////////////////////////////////////////////////////////////
-    function get_PName(){
-        DbConnection::getConnection();
+  public  function get_PName(){
+
         $sql = "Select p_name FROM products where p_id='".$_POST["p_id"]."'";
 
-        if ( DbConnection::getConnection()->query($sql) === TRUE) {
+        if ( $this->dbConnection->query($sql)->query($sql) === TRUE) {
             echo "Query success";
-            $result = DbConnection::getConnection()->query($sql);
+            $result = $this->dbConnection->query($sql)->query($sql);
             if ($result->num_rows > 0) {
                 // output data of each row
                 while($row = $result->fetch_assoc()) {
@@ -158,6 +118,18 @@ function Get_Products(){
 
 
     }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+   public function checkstatus($value){
+    $sql="select status from products where p_id='".$value."'";
+     $result=$this->dbConnection->query($sql);
+     return $result;
+ }
+ ///////////////////////////////////////////////////////////////////////////////////////////
+ public function changestatus($id,$value){
+$sql = "UPDATE products SET  status='".$value."' where p_id='".$id."'";
+$result=$this->dbConnection->query($sql);
+return$result;
+ } 
 
 
 }
