8a59bbb7bd8e5830194d66c3b72540df42f2cb92
AyaMohammed <engayasharafelden@gmail.com>
2016-02-18 21:47:10 +0200

    Update Sumbit.php

--- models/Sumbit.php
+++ models/Sumbit.php
@@ -3,33 +3,30 @@ require_once "DbConnection.php";
 require_once "Product.php";
 
 $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
-//echo $DOCUMENT_ROOT;
-// put the file where we’d like it
-$upfile = '$DOCUMENT_ROOT/Project_Php/img/'.$_FILES['p_img']['tmp_name'];
-
+$upfile = "$DOCUMENT_ROOT/Project_Php/img/".$_FILES['p_img']['name'] ;
 // Does the file have the right MIME type?
 if ($_FILES['p_img']['type'] != 'image/jpeg')
 {
-    echo 'Problem: file is not Image';
-    exit;
+echo 'Problem: file is not plain text';
+exit;
 }
-if (is_uploaded_file($_FILES['p_img']['tmp_name'])) {
-    if (!move_uploaded_file($_FILES['p_img']['tmp_name'], $upfile)) {
-        echo 'Problem: Could not move file to destination directory';
-        exit;
-    } else {
-        echo 'Problem: Possible file upload attack. Filename: ';
-        echo $_FILES['p_img']['name'];
-        exit;
-    }
-    echo 'File uploaded successfully<br><br>';
-
+if (is_uploaded_file($_FILES['p_img']['tmp_name']))
+{
+if (!move_uploaded_file($_FILES['p_img']['tmp_name'], $upfile))
+{
+echo 'Problem: Could not move file to destination directory';
+ }
+else
+{
+echo 'Problem: Possible file upload attack. Filename: ';
+echo $_FILES['p_img']['name'];
 
 }
-
+//echo 'File uploaded successfully<br><br>';
+}
 $pro = new Product(DbConnection::getConnection("localhost","aya","aya","cafteria"));
 $result = $pro->insert_product();
-var_dump($result->fetch_assoc());
+header("location: Add_product.php");
 /**
  * Created by PhpStorm.
  * User: ayasharafelden
