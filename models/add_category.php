171bdf32b25c337e49f48aa46ab798c259b60008
AyaMohammed <engayasharafelden@gmail.com>
2016-02-19 18:11:20 +0200

    Create add_category.php

---
+++ models/add_category.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../controllers/DbConnection.php";
+require_once "../controllers/Category.php";
+/**
+ * Created by PhpStorm.
+ * User: ayasharafelden
+ * Date: 2/15/16
+ * Time: 12:00 AM
+ */
+
+$cat=new Category(DbConnection::getConnection("localhost","aya","aya","cafteria"));
+
+$result = $cat->insert_product();
+echo "jjdjd";
+var_dump($result->fetch_assoc());
+echo $result;
