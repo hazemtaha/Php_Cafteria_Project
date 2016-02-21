a563f88816037a7c48d655f7681df729766eaba3
AyaMohammed <engayasharafelden@gmail.com>
2016-02-21 21:55:50 +0200

    Update img.php

--- models/img.php
+++ models/img.php
@@ -15,23 +15,37 @@ if (is_uploaded_file($_FILES['pic']['tmp_name']))
 		if (!move_uploaded_file($_FILES['pic']['tmp_name'], $upfile))
 		{
 		echo 'Problem: Could not move file to destination directory';
-		exit;
+		//exit;
 		 }
 		else
 		{
-		//echo 'Problem: Possible file upload attack. Filename: ';
+		echo 'Problem: Possible file upload attack. Filename: ';
 		//echo $_FILES['pic']['name'];
 		}
 //echo 'File uploaded successfully<br><br>';
 }
+if($_POST["u_pass"]!==$_POST["con_pass"]){
+    header("location: ../views/AddUser.php?id=1");
 
+}
+ if (!is_numeric($_POST["u_room"])) {
+         header("location: ../views/AddUser.php?id=2");
+    } 
+  if(!is_numeric($_POST["ext"])) {
+         header("location: ../views/AddUser.php?id=3");}
 
-	
+if($_POST["u_pass"]===$_POST["con_pass"] and is_numeric($_POST["u_room"]) and is_numeric($_POST["ext"])){
 
-
-$pro = new user(DbConnection::getConnection("localhost","zahra","iti","cafteria"));
+$pro = new user(DbConnection::getConnection("localhost","aya","aya","cafteria"));
 $result = $pro->Add_user();
-header("location: ../views/add-user.html");
+header("location: ../views/AddUser.php");
+
+
+
+
+}	
+
+
 
 
 
