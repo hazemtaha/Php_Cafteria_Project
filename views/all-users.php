58fcf1f155d4c51eeb465c691d85b805fd40797a
AyaMohammed <engayasharafelden@gmail.com>
2016-02-21 21:13:32 +0200

    Update all-users.php

--- views/all-users.php
+++ views/all-users.php
@@ -1,51 +1,105 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>All Users</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-<body>
 
-<div class="col-sm-12">
-  <nav class="navbar navbar-inverse">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <style >
+        .well{
+            background-color: black;
+        }
+        .col-sm-2{
+
+            padding: 10px;
+ }
+        .shape{
+            border: 1px solid Gainsboro  ;
+  }
+  table{
+
+      
+
+
+  }
+
+
+.col-sm-4 {
+
+
+     border: 1px solid Gainsboro;
+
+}
+tr , td{
+
+  padding: 7px;
+
+
+}
+
+#size{
+
+
+    border: 1px;
+}
+
+
+    </style>
+
+    <title>Show Products</title>
+</head>
+<body  style="background-color:Snow ">
+<nav class="navbar navbar-inverse ">
     <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#">Cafeteria</a>
-      </div>
-      <ul class="nav navbar-nav">
+        <div class="navbar-header">
+            <img src="../assets/img/start.jpg" width="50" heigth="50"/>
+
+        </div>
+        <div class="navbar-header">
+
+            <a class="navbar-brand" href="#">Cafertaria</a>
+
+        </div>
+        <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
         <li><a href="#">Products</a></li>
         <li><a href="#">Users</a></li>
         <li><a href="#">Manual Order</a></li>
         <li><a href="#">Checks</a></li>
         <li><a href="#">All Products</a></li>
-        <li><img src=""/></li>
-        <li><a href="#">Admin</a></li>
+       
       </ul>
-    </div>
+      <ul>
+      <ul class="nav navbar-nav navbar-right">
+            <li><img src="../assets/img/start.jpg" width="50" height="50"/> </li>
+            <li></li>
+            <li><a href="#">Islam Asker</a></li>
+            <li><a href="#">Logout</a></li>
+        </ul>
+      </ul>
+
   </nav>
 </div>
 
 
 
-<br> <br> <br> <br>
+<br> <br> 
 
 
-  <div class="col-sm-11" >
-    <h1> All Users </h1>
+  <div class="col-sm-11" align="center">
+    <h1 style="color:Salmon ;font-style: oblique;"> All Users </h1>
   </div>
   <div class="col-sm-1" >
-      <a href="#">Add user</a>
+      <a href="AddUser.php">Add user</a>
   </div>
 
 <br> <br> <br> <br>
 
 <div class="col-sm-12 table-responsive">
- <table class="table table-condensed">
+ <table class="table table-condensed  table-hover">
     <thead>
       <tr>
         <th>Name</th>
@@ -61,7 +115,7 @@
 
         require_once "../controllers/DbConnection.php";
         require_once "../controllers/user.php";
-        $pro = new user(DbConnection::getConnection("localhost","zahra","iti","cafteria"));
+        $pro = new user(DbConnection::getConnection("localhost","aya","aya","cafteria"));
         $result = $pro->dispaly_users();
 
       if ($result->num_rows > 0) 
@@ -92,14 +146,9 @@
 
 <br> <br> <br> <br>
 
-<div class=" col-sm-12 text-center">
-  <ul class="pagination">
-    <li><a href="#">1</a></li>
-    <li class="active"><a href="#">2</a></li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">4</a></li>
-    <li><a href="#">5</a></li>
-  </ul>
- </div>
+ <div class=" row  text-center" background="Black">
+      <div class="well">Insititute of Information and Technology </div>
+</div>
+
 </body>
 </html>
